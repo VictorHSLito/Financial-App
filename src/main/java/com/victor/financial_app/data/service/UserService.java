fab9bfdb016f4e4167dfbede794e840f8cc2c573
Victor Hugo <victorferrarihugo@hotmail.com>
2025-05-16 11:19:17 -0300

    feat: Create endpoints to delete one user, get one user, and get all users

--- src/main/java/com/victor/financial_app/data/service/UserService.java
+++ src/main/java/com/victor/financial_app/data/service/UserService.java
@@ -7,6 +7,8 @@ import com.victor.financial_app.presentation.controllers.UserDTO;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,7 +20,7 @@ public class UserService {
     }
 
     public Long createUser(UserDTO userDTO) {
-
+        //Método que cria um usuário no banco de dados
         User entity = new User(
                 userDTO.username(),
                 userDTO.email(),
@@ -29,5 +31,28 @@ public class UserService {
         User userSaved = userRepository.save(entity);
 
         return userSaved.getId();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        //Método que retorna um usuário no banco de dados pelo id
+        var user = userRepository.findById(id);
+        return user;
+    }
+
+    public List<User> getAllUsers() {
+        //Método que lista todos os usuários no banco de dados
+        return userRepository.findAll();
+    }
+
+    public void deleteUserById(Long id) {
+        //Método que deleta um usuário no banco de dados pelo id
+        var user = userRepository.findById(id);
+
+        if (user.isPresent()) {
+            userRepository.deleteById(user.get().getId());
+        }
+        else {
+            System.out.println("User doesn't exists!");
+        }
     }
 }
--- src/main/java/com/victor/financial_app/presentation/controllers/UserController.java
+++ src/main/java/com/victor/financial_app/presentation/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -19,13 +20,34 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO) {
+        //Endpoint responsável pela criação de um usuário
         Long id = userService.createUser(userDTO);
         return ResponseEntity.created(URI.create("/users/" + id.toString())).build();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable("id") String id) {
-        return null;
+        //Endpoint responsável por retornar ou não um usuário
+        var user = userService.getUserById(Long.parseLong(id));
+        if (user.isPresent()) {
+            var u = user.get();
+            return ResponseEntity.ok(u);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> getUsers() {
+        //Endpoint responsável por retornar todos os usuários
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable("id") String id){
+        //Endpoint responsável por deletar um usuário pelo id
+        userService.deleteUserById(Long.parseLong(id));
+        return ResponseEntity.noContent().build();
     }
 }
 
