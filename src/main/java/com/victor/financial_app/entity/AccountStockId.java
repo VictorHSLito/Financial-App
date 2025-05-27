50cf57346a2fc1ff5fcc8152e09990535d8e0dde
Victor Hugo <victorferrarihugo@hotmail.com>
2025-05-27 11:24:51 -0300

    feat: Create AccountStock and AccountStockId Entities

---
+++ src/main/java/com/victor/financial_app/entity/AccountStockId.java
@@ -0,0 +1,24 @@
+package com.victor.financial_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.UUID;
+
+@Embeddable
+public class AccountStockId {
+
+    @Column(name = "account_id")
+    private UUID accountId;
+
+    @Column(name = "stock_id")
+    private String stockId;
+
+    public AccountStockId(UUID accountId, String stockId) {
+        this.accountId = accountId;
+        this.stockId = stockId;
+    }
+
+    public AccountStockId() {
+    }
+}
