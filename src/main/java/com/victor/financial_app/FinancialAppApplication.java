3d977c12529cad6bcaf167be5038ebb92bf3f0ca
Victor Hugo <victorferrarihugo@hotmail.com>
2025-05-29 22:36:03 -0300

    build: Update dependencies and finish the project

--- src/main/java/com/victor/financial_app/FinancialAppApplication.java
+++ src/main/java/com/victor/financial_app/FinancialAppApplication.java
@@ -2,8 +2,10 @@ package com.victor.financial_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FinancialAppApplication {
 
 	public static void main(String[] args) {
